bfa4e7c20e87da1c5f75b112dca110864b100299
William Newman <wnewman@catalyte.io>
2019-05-09 14:51:12 -0400

    Basic structure impl, meters impl

--- src/app/options/opt-one/opt-one.component.ts
+++ src/app/options/opt-one/opt-one.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./opt-one.component.css']
 })
 export class OptOneComponent implements OnInit {
+  options: string[];
 
   constructor() { }
 
   ngOnInit() {
+    this.options = ['yee', 'woo', 'good'];
+  }
+
+  addItem() {
+    this.options.push('');
   }
 
 }
